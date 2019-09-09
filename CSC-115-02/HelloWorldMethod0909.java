c851fe4e94ec409f526b88060d4de48118bba0ed
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2019-09-09 14:45:15 -0400

    Create HelloWorldMethod0909.java

---
+++ CSC-115-02/HelloWorldMethod0909.java
@@ -0,0 +1,16 @@
+// Carrie Krueger
+// 9/4/2019
+// This program writes a message to the screen.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+       printMessage(); // calls the method to print the message
+
+    }
+    
+    //this method prints a message to the screen
+    public static void printMessage() {
+         System.out.println("Hello, World!"); //writes to the screen
+    }
+}
