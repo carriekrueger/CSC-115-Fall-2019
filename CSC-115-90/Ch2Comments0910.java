1224828e71cf6e27fc2751127f368f85161f35ec
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2019-09-10 17:08:21 -0400

    Create Ch2Comments0910.java

---
+++ CSC-115-90/Ch2Comments0910.java
@@ -0,0 +1,71 @@
+// Carrie Krueger
+// 9-10-19
+// Chapter 2 Notes: Comments
+
+public class Ch2Comments {
+    public static void main(String[] args) {
+        
+        comments();  // in this method we will explore comments
+    }
+    
+    // a method to investigate comments
+    public static void comments() {
+        // anything you type in a comment is ignored by the compiler/computer
+        
+        // We comments for:
+        // -Adding your name to the program 
+        // -Adding a brief description of the program 
+        // -As you go along, explain to others (and yourself) what your code should do
+        
+        // Also... DEBUGGING
+        // If there is code that doesn't quite work yet...
+        // If there are a bunch of lines of code that have an error that you want to 
+        // isolate
+        
+        // GIVE CREDIT 
+        // If you use someone else's code, use comments to credit them and include 
+        // the place where you got it from 
+        // ex. shamelessy stolen from http://github.com/awesomecoder
+        
+        // HOW DO WE MAKE COMMENTS?
+        // Line comments: anything after the // is ignored by the compiler
+        // Block comment:  /*  */ (slash star star slash) (comment sandwich)
+        // Java Doc comments: /** */ (will automatically generate documentation- 
+        // we won't use much)
+        
+        // Examples
+        
+        /* When you have multiple lines of comments
+           it's helpful to do a block comment because
+           you can add as many lines as you want.
+           Good for use at top - name/description.
+        */
+        
+        // help with debugging:
+        
+        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        
+        /*
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+          
+        */
+        
+        
+        
+
+        
+        
+    }
+}
